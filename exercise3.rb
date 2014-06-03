da7e1836f86b1565ca05eaad8ede6781f5ae5704
davidhurley <dhurls99@gmail.com>
2014-06-03 11:28:10 -0400

    finished exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,11 @@
 puts "What is your name?"
+name = gets.chomp
+puts "Hi, #{name}"
+
+require 'date'
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+today = DateTime.now.year - age
+puts "You were born in #{today} unless you haven't had your birthday yet. In that case you were born in #{today - 1}"
