ee0c40d3088e7142f3bc050dd5aa9ca5e98bc469
davidhurley <dhurls99@gmail.com>
2014-06-03 11:13:30 -0400

    just finished exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,2 +1,13 @@
 #calculate a good tip
 puts 55*0.2
+
+#adding string to integer
+puts "dave" + 5.to_s
+
+#multiply 2 numbers together using string interpolation
+n1 = 45628
+n2 = 7839
+
+puts "#{n1} mutliplied by #{n2} is #{n1*n2}"
+
+puts (true && false) || (false && true) || !(false && false)
